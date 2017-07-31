b47459cfa812ae3095525c2437ef1b02757b2c9f
Ankit Agarwal1 <Ankit.Agarwal1@rsystems.com>
2017-07-31 14:05:00 +0200

    confiure buind version inside POM.xml

--- src/test/java/com/rsystems/utils/ReportGeneration.java
+++ src/test/java/com/rsystems/utils/ReportGeneration.java
@@ -17,7 +17,11 @@ public class ReportGeneration {
 
 	private String generateOptionsValue(final File folder) throws IOException {
 
-		String buildVersion = TestInitization.getUpdatedProptiesFile().getProperty("BuildVersion");
+		String buildVersion = null;
+		buildVersion = System.getProperty("BuildVersion");
+		if (buildVersion == null || buildVersion.contentEquals("")) {
+			buildVersion = TestInitization.getUpdatedProptiesFile().getProperty("BuildVersion");
+		}
 
 		String optionValues = "";
 		for (File folder1 : folder.listFiles()) {
